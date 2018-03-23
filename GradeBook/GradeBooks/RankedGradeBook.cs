85febbb094dbfd95a58f5f8bc42bedc604407db9
June Panic <june_panic@yahoo.com>
2018-03-22 22:22:49 -0500

    Removed debugging statements

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -20,30 +20,17 @@ namespace GradeBook.GradeBooks
 
             List<Student> sortedByGrades = Students.OrderByDescending(o => o.AverageGrade).ToList();
             double numStudentsPer20Percent = sortedByGrades.Count * .2;
-            Console.WriteLine("numStudentsPer20Percent {0}", numStudentsPer20Percent);
             int rankedGrade = sortedByGrades.FindIndex(a => a.AverageGrade == averageGrade) + 1;
-            Console.WriteLine("rankedGrade {0}", rankedGrade);
 
             if (rankedGrade <= numStudentsPer20Percent)
-            {
-                Console.WriteLine("A");
                 return 'A';
-            }
             else if(rankedGrade <= numStudentsPer20Percent * 2)
-            {
-                Console.WriteLine("A");
                 return 'B';
-            }
             else if(rankedGrade <= numStudentsPer20Percent * 3)
-            {
-                Console.WriteLine("A");
                 return 'C';
-            }
             else if(rankedGrade <= numStudentsPer20Percent * 4)
-            {
-                Console.WriteLine("A");
                 return 'D';
-            }
+
             return 'F';
         }
     }
